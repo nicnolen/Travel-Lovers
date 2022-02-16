dfc748fecfedff0636899f0ddb9653c943eff66c
Nicholas Nolen <nicnolen@ymail.com>
2022-02-16 11:43:36 -0600

    set up initial heroku server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 //handlebar setup - Christian Ramirez
 const exphbs = require('express-handlebars');
