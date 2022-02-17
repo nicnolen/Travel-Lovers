3f16fdb80a3b568d1e26e24ae44d34f71e4f8743
hoapham96 <89959754+hoapham96@users.noreply.github.com>
2022-02-17 15:31:29 -0600

    connect sequelize library, and user-routes API

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// GET /api/users
+router.get('/', (req, res) => {});
+
+// GET /api/users/1
+router.get('/:id', (req, res) => {});
+
+// POST /api/users
+router.post('/', (req, res) => {});
+
+// PUT /api/users/1
+router.put('/:id', (req, res) => {});
+
+// DELETE /api/users/1
+router.delete('/:id', (req, res) => {});
+
+module.exports = router;
