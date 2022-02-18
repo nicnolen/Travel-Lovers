844c40e5d53e674e954c2a6bb2b37297a4ddcf8c
Nicholas Nolen <nicnolen@ymail.com>
2022-02-18 13:18:49 -0600

    set up initial structure for signup form handler in javascript

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,2 +1,12 @@
 /* LOGIN PAGE FRONT END LOGIC */
 
+// Signup form handler
+function signupFormHandler(event) {
+  event.preventDefault();
+}
+
+// EVENT LISTENERS
+// Signup event listener
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
