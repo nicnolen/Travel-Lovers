38888c35296507e87632c1bcb692b3ff37f1425d
Nicholas Nolen <nicnolen@ymail.com>
2022-02-18 12:57:37 -0600

    added login handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,9 +2,15 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Place, Comment } = require('../models');
 
+// GET homepage for Place
 router.get('/', (req, res) => {
-    Place.findAll({})
-    res.render('homepage')
-})
+  Place.findAll({});
+  res.render('homepage');
+});
+
+// GET route to login page
+router.get('/login', (req, res) => {
+  res.render('login');
+});
 
 module.exports = router;
