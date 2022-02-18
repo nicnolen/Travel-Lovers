df28c5c752744c0795c5d26a86e3ba22ece81be8
Nicholas Nolen <nicnolen@ymail.com>
2022-02-18 10:03:12 -0600

    added initial logout route to destroy sessions

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -102,6 +102,9 @@ router.post('/login', (req, res) => {
   });
 });
 
+// POST /api/users/logout
+router.post('/logout', (req, res) => {});
+
 // PUT /api/users/1 (update user by id)
 router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
