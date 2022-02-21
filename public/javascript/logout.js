71db278e7db810c2ccfe03fc83ac9e0de518b0db
Nicholas Nolen <nicnolen@ymail.com>
2022-02-21 14:09:38 -0600

    added error handling to the logout.js file in the public folder

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,8 +1,19 @@
 /* LOGOUT PAGE */
+// Reference error messages
+const errorEl = document.getElementById('error-message');
+
 // Event handler for logout route
 async function logout() {
   const response = await fetch('/api/user/logout', {
     method: 'post',
     headers: { 'Content-Type': 'application.json' },
   });
+
+  if (response.ok) {
+    // clear error messages
+    errorEl.innerHTML = '';
+    document.location.replace('/');
+  } else {
+    errorEl.innerHTML = response.statusText;
+  }
 }
