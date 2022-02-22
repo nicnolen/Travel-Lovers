3b0056b6174e98e2eb64bc3d3d97ecd73cc1cb04
Nicholas Nolen <nicnolen@ymail.com>
2022-02-22 08:05:03 -0600

    added in signup form handler function

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,8 +1,35 @@
 /* LOGIN PAGE FRONT END LOGIC */
+// Reference error messages
+const errorEl = document.getElementById('error-message');
 
 // Signup form handler
-function signupFormHandler(event) {
+async function signupFormHandler(event) {
   event.preventDefault();
+
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  // If there is a username, email and password then make a post method
+  if (username && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    // check response status
+    if (response.ok) {
+      errorEl.innerHTML = '';
+      document.location.replace('/dashboard/');
+    } else {
+      errorEl.innerHTML = response.statusText;
+    }
+  }
 }
 
 // EVENT LISTENERS
