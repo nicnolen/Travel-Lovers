9b0a6d1306210d7364313315acd7f21490d815c0
hoapham96 <89959754+hoapham96@users.noreply.github.com>
2022-02-24 17:44:25 -0600

    add heroku database

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,14 @@
 // import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
+const dbConfig = require("./db.connection.js");
 
 require('dotenv').config();
 
 // create connection to our database, pass in your MySQL information for username and password
 const sequelize = new Sequelize(
-  process.env.DB_NAME, 
-  process.env.DB_USER, 
-  process.env.DB_PW, {
+  process.env.DB_NAME || dbConfig.DB, 
+  process.env.DB_USER || dbConfig.USER, 
+  process.env.DB_PW || dbConfig.PASSWORD, {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306
---
+++ config/db.connection.js
@@ -0,0 +1,6 @@
+module.exports = {
+    HOST: "us-cdbr-east-05.cleardb.net",
+    USER: "bc3ed62a6bd91b",
+    PASSWORD: "03028a2c",
+    DB: "heroku_18dc4e67a447388"
+  };
