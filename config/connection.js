883b6b96f9834832178bfc4accbf86df14ae6a56
hoapham96 <89959754+hoapham96@users.noreply.github.com>
2022-02-24 17:52:03 -0600

    fix

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
 // import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
-const dbConfig = require("./db.connection.js");
+const dbConnection = require("./db.connection.js");
 
 require('dotenv').config();
 
 // create connection to our database, pass in your MySQL information for username and password
 const sequelize = new Sequelize(
-  process.env.DB_NAME || dbConfig.DB, 
-  process.env.DB_USER || dbConfig.USER, 
-  process.env.DB_PW || dbConfig.PASSWORD, {
+  process.env.DB_NAME || dbConnection.DB, 
+  process.env.DB_USER || dbConnection.USER, 
+  process.env.DB_PW || dbConnection.PASSWORD, {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306
