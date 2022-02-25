170f83ed843a0578354fc8f9cb4d95057ecd2857
Nicholas Nolen <nicnolen@ymail.com>
2022-02-25 11:14:59 -0600

    made travel information buttons for each city input and styled

--- public/javascript/dashboard.js
+++ public/javascript/dashboard.js
@@ -1,17 +1,19 @@
-
-
-function addItem(){
-    var ul = document.getElementById("dynamic-list");
-    var candidate = document.getElementById("candidate");
-    var li = document.createElement("li");
-    var calendar = document.getElementById("datepicker");
-    li.setAttribute('id',candidate.value);
-    li.setAttribute('cal', calendar.value);
-    li.appendChild(document.createTextNode(candidate.value + " "));
-    li.appendChild(document.createTextNode(calendar.value));
-    ul.appendChild(li);
+function addItem() {
+  var ul = document.getElementById('dynamic-list');
+  var candidate = document.getElementById('candidate');
+  var li = document.createElement('li');
+  var calendar = document.getElementById('datepicker');
+  var button = document.createElement('button');
+  li.setAttribute('id', candidate.value);
+  li.setAttribute('cal', calendar.value);
+  li.appendChild(document.createTextNode(candidate.value + ' '));
+  li.appendChild(document.createTextNode(calendar.value));
+  button.innerHTML = '<br>';
+  button.textContent = 'Travel Information';
+  button.classList.add('button', 'btn-dashboard');
+  li.appendChild(button);
+  ul.appendChild(li);
 }
-    
 
 // Do something with your data.
 // function loadStuff() {
@@ -20,48 +22,57 @@ function addItem(){
 //     }
 // }
 
-function removeItem(){
-    var ul = document.getElementById("dynamic-list");
-    var candidate = document.getElementById("candidate");
-    var item = document.getElementById(candidate.value);
-    ul.removeChild(item);
+function removeItem() {
+  var ul = document.getElementById('dynamic-list');
+  var candidate = document.getElementById('candidate');
+  var item = document.getElementById(candidate.value);
+  ul.removeChild(item);
 }
 
-function addItem1(){
-    var ul = document.getElementById("dynamic-list1");
-    var candidate = document.getElementById("candidate1");
-    var li = document.createElement("li");
-    var calendar = document.getElementById("datepicker1");
-    li.setAttribute('id',candidate.value);
-    li.setAttribute('cal', calendar.value);
-    li.appendChild(document.createTextNode(candidate.value + " "));
-    li.appendChild(document.createTextNode(calendar.value));
-    ul.appendChild(li);
+function addItem1() {
+  var ul = document.getElementById('dynamic-list1');
+  var candidate = document.getElementById('candidate1');
+  var li = document.createElement('li');
+  var calendar = document.getElementById('datepicker1');
+  var button = document.createElement('button1');
+  li.setAttribute('id', candidate.value);
+  li.setAttribute('cal', calendar.value);
+  li.appendChild(document.createTextNode(candidate.value + ' '));
+  li.appendChild(document.createTextNode(calendar.value));
+  button.innerHTML = '<br>';
+  button.textContent = 'Travel Information';
+  button.classList.add('button', 'btn-dashboard');
+  li.appendChild(button);
+  ul.appendChild(li);
 }
 
-function removeItem1(){
-    var ul = document.getElementById("dynamic-list1");
-    var candidate = document.getElementById("candidate1");
-    var item = document.getElementById(candidate.value);
-    ul.removeChild(item);
+function removeItem1() {
+  var ul = document.getElementById('dynamic-list1');
+  var candidate = document.getElementById('candidate1');
+  var item = document.getElementById(candidate.value);
+  ul.removeChild(item);
 }
 
-function addItem2(){
-    var ul = document.getElementById("dynamic-list2");
-    var candidate = document.getElementById("candidate2");
-    var li = document.createElement("li");
-    var calendar = document.getElementById("datepicker2");
-    li.setAttribute('id',candidate.value);
-    li.setAttribute('cal', calendar.value);
-    li.appendChild(document.createTextNode(candidate.value + " "));
-    li.appendChild(document.createTextNode(calendar.value));
-    ul.appendChild(li);
+function addItem2() {
+  var ul = document.getElementById('dynamic-list2');
+  var candidate = document.getElementById('candidate2');
+  var li = document.createElement('li');
+  var calendar = document.getElementById('datepicker2');
+  var button = document.createElement('button2');
+  li.setAttribute('id', candidate.value);
+  li.setAttribute('cal', calendar.value);
+  li.appendChild(document.createTextNode(candidate.value + ' '));
+  li.appendChild(document.createTextNode(calendar.value));
+  button.innerHTML = '<br>';
+  button.textContent = 'Travel Information';
+  button.classList.add('button', 'btn-dashboard');
+  li.appendChild(button);
+  ul.appendChild(li);
 }
 
-function removeItem2(){
-    var ul = document.getElementById("dynamic-list2");
-    var candidate = document.getElementById("candidate2");
-    var item = document.getElementById(candidate.value);
-    ul.removeChild(item);
+function removeItem2() {
+  var ul = document.getElementById('dynamic-list2');
+  var candidate = document.getElementById('candidate2');
+  var item = document.getElementById(candidate.value);
+  ul.removeChild(item);
 }
-
