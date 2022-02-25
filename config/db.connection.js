5b0e1c9ddaff6204cc354f23eba133cfaefb8780
hoapham96 <89959754+hoapham96@users.noreply.github.com>
2022-02-24 18:38:30 -0600

    deploy

--- config/db.connection.js
+++ config/db.connection.js
@@ -1,6 +1,6 @@
 module.exports = {
     HOST: "us-cdbr-east-05.cleardb.net",
-    USER: "bc3ed62a6bd91b",
-    PASSWORD: "03028a2c",
-    DB: "heroku_18dc4e67a447388"
+    USER: "bbfb9e375921eb",
+    PASSWORD: "6076cf4f",
+    DB: "heroku_c56645ec48a49c9"
   };
